482c13a2765e1bcba678e8512c627f19f8d2d9e4
FurkanAydnn <furkanaydn67@gmail.com>
2019-02-13 22:26:35 +0300

    Login ve Register Güncellendi

--- FurBuuks/FurBuuks/Controllers/LoginController.cs
+++ FurBuuks/FurBuuks/Controllers/LoginController.cs
@@ -16,23 +16,23 @@ namespace FurBuuks.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Index(User u)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Users.Add(u);
-                db.SaveChanges();
-                return Redirect("/User/UserEdit/" + u.Id);
-            }
+        //[HttpPost]
+        //public ActionResult Index(User u)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Users.Add(u);
+        //        db.SaveChanges();
+        //        return Redirect("/User/UserEdit/" + u.Id);
+        //    }
 
-            return View();
-        }
+        //    return View();
+        //}
 
         [HttpPost]
         public JsonResult ValidateUser(string email, string password)
         {
-            var data = from c in db.Users where c.Email == email && c.Password == password select c;
+            var data = from c in db.Users where (c.Email == email || c.UserName == email) && c.Password == password select c;
             var selected = data.FirstOrDefault();
             if (data.Count() > 0)
             {
@@ -41,6 +41,38 @@ namespace FurBuuks.Controllers
             }
             else
                 return Json(new { Success = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult UserSignUp(User user, string email, string password, string username, string passwordCheck)
+        {
+            if (password == passwordCheck)
+            {
+                var data = from u in db.Users where u.Email == email || u.UserName == username select u;
+                if (!(data.Count() > 0))
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    Session["User"] = user;
+                    return Json(new { Success = true }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    var emails = from u in db.Users where u.Email == email select u.Email;
+                    //var usernames = from u in db.Users where u.UserName == username select u.UserName;
+                    if (emails.Contains(email))
+                    {
+                        return Json(new { Success = false, Error = "Bu email zaten kullanılıyor." }, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        return Json(new { Success = false, Error = "Bu kullanıcı adı zaten kullanılıyor." }, JsonRequestBehavior.AllowGet);
+                    }
+                }
+            }
+            else
+                return Json(new { Success = false, Error = "Girdiğiniz şifreler uyuşmuyor." }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
